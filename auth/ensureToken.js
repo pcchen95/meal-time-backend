6b9c79a8b2122dc839f559c950c3ed16a8aea26d
peichuan <taco00766@gmail.com>
2021-10-05 15:47:56 +0800

    modify ensureToken

--- auth/ensureToken.js
+++ auth/ensureToken.js
@@ -1,19 +1,24 @@
 const ensureToken = (req, res, next) => {
   const bearerHeader = req.headers.authorization;
   if (!bearerHeader) {
-    return res.status(403).send(
-      Object.assign(
-        { code: 403 },
-        {
-          ok: 0,
-          message: 'Please login.',
-        }
-      )
+    return res.status(401).send(
+      Object.assign({
+        ok: 0,
+        message: 'Please login.',
+      })
     );
   }
 
   const bearer = bearerHeader.split(' ');
   const bearerToken = bearer[1];
+  if (!bearerToken) {
+    return res.status(401).send(
+      Object.assign({
+        ok: 0,
+        message: 'you are not authorized',
+      })
+    );
+  }
   req.token = bearerToken;
   return next();
 };
