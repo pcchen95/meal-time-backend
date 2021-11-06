b7c8c86dab4601f5b96d40b4a0cfa7b04518e19b
peichuan <taco00766@gmail.com>
2021-11-07 02:34:52 +0800

    fix currentTime

--- controllers/product.js
+++ controllers/product.js
@@ -1,14 +1,14 @@
-const { Op } = require('sequelize')
-const db = require('../models')
-const { uploadImg, deleteImg } = require('./imgur.js')
-const jwt = require('jsonwebtoken')
-const secretKey = require('../auth/secretKey')
-const { Product, ProductCategory, Vendor, VendorCategory, User } = db
-const album = 'gpxFA0k'
+const { Op } = require('sequelize');
+const db = require('../models');
+const { uploadImg, deleteImg } = require('./imgur.js');
+const jwt = require('jsonwebtoken');
+const secretKey = require('../auth/secretKey');
+const { Product, ProductCategory, Vendor, VendorCategory, User } = db;
+const album = 'gpxFA0k';
 
 const productController = {
   getInfo: async (req, res) => {
-    const id = req.params.id
+    const id = req.params.id;
     try {
       const procuct = await Product.findOne({
         where: {
@@ -39,47 +39,44 @@ const productController = {
             ],
           },
         ],
-      })
+      });
       return res.status(200).json({
         ok: 1,
         data: procuct,
-      })
+      });
     } catch (err) {
       return res.status(500).json({
         ok: 0,
         message: err.toString(),
-      })
+      });
     }
   },
 
   getByVendorManage: async (req, res) => {
-    const id = Number(req.params.id)
-    let { page, limit, sort, order, category, filter } = req.query
-    const _page = Number(page) || 1
-    const _limit = limit ? parseInt(limit) : null
-    const _offset = (_page - 1) * _limit
-    const _sort = sort || 'id'
-    const _order = order || 'DESC'
-    const _category = category || null
-    const _filter = filter || 'all'
-    const now = new Date()
-    now.setHours(0)
-    now.setMinutes(0)
-    now.setSeconds(0)
-    const today = now.getTime()
+    const id = Number(req.params.id);
+    let { page, limit, sort, order, category, filter } = req.query;
+    const _page = Number(page) || 1;
+    const _limit = limit ? parseInt(limit) : null;
+    const _offset = (_page - 1) * _limit;
+    const _sort = sort || 'id';
+    const _order = order || 'DESC';
+    const _category = category || null;
+    const _filter = filter || 'all';
+    const now = new Date().toUTCString();
+    const today = new Date(now).getTime();
 
     jwt.verify(req.token, secretKey, async (err, decoded) => {
       if (err) {
         return res.status(500).json({
           ok: 0,
           message: err.toString(),
-        })
+        });
       }
       if (decoded.payload.vendorId !== id) {
         return res.status(401).json({
           ok: 0,
           message: 'you are not authorized',
-        })
+        });
       }
       try {
         const procucts = await Product.findAndCountAll({
@@ -123,36 +120,33 @@ const productController = {
           ...(_limit && { limit: _limit }),
           offset: _offset,
           order: [[_sort, _order]],
-        })
+        });
 
         return res.status(200).json({
           ok: 1,
           data: procucts,
-        })
+        });
       } catch (err) {
         return res.status(500).json({
           ok: 0,
           message: err.toString(),
-        })
+        });
       }
-    })
+    });
   },
 
   getByVendor: async (req, res) => {
-    const id = req.params.id
-    let { page, limit, sort, order, category, notSupplied } = req.query
-    const _page = Number(page) || 1
-    const _limit = limit ? parseInt(limit) : null
-    const _offset = (_page - 1) * _limit
-    const _sort = sort || 'id'
-    const _order = order || 'DESC'
-    const _category = category || null
-    const _notSupplied = Boolean(notSupplied) || false
-    const now = new Date()
-    now.setHours(0)
-    now.setMinutes(0)
-    now.setSeconds(0)
-    const today = now.getTime()
+    const id = req.params.id;
+    let { page, limit, sort, order, category, notSupplied } = req.query;
+    const _page = Number(page) || 1;
+    const _limit = limit ? parseInt(limit) : null;
+    const _offset = (_page - 1) * _limit;
+    const _sort = sort || 'id';
+    const _order = order || 'DESC';
+    const _category = category || null;
+    const _notSupplied = Boolean(notSupplied) || false;
+    const now = new Date().toUTCString();
+    const today = new Date(now).getTime();
     try {
       const procucts = await Product.findAndCountAll({
         where: {
@@ -190,34 +184,31 @@ const productController = {
         ...(_limit && { limit: _limit }),
         offset: _offset,
         order: [[_sort, _order]],
-      })
+      });
 
       return res.status(200).json({
         ok: 1,
         data: procucts,
-      })
+      });
     } catch (err) {
       return res.status(500).json({
         ok: 0,
         message: err.toString(),
-      })
+      });
     }
   },
 
   getByCategory: async (req, res) => {
-    const id = req.params.id
-    let { page, limit, sort, order, notSupplied } = req.query
-    const _page = Number(page) || 1
-    const _limit = limit ? parseInt(limit) : 10
-    const _offset = (_page - 1) * _limit
-    const _sort = sort || 'id'
-    const _order = order || 'DESC'
-    const _notSupplied = Boolean(notSupplied) || false
-    const now = new Date()
-    now.setHours(0)
-    now.setMinutes(0)
-    now.setSeconds(0)
-    const today = now.getTime()
+    const id = req.params.id;
+    let { page, limit, sort, order, notSupplied } = req.query;
+    const _page = Number(page) || 1;
+    const _limit = limit ? parseInt(limit) : 10;
+    const _offset = (_page - 1) * _limit;
+    const _sort = sort || 'id';
+    const _order = order || 'DESC';
+    const _notSupplied = Boolean(notSupplied) || false;
+    const now = new Date().toUTCString();
+    const today = new Date(now).getTime();
     try {
       const procucts = await Product.findAndCountAll({
         where: {
@@ -254,32 +245,29 @@ const productController = {
         limit: _limit,
         offset: _offset,
         order: [[_sort, _order]],
-      })
+      });
       return res.status(200).json({
         ok: 1,
         data: procucts,
-      })
+      });
     } catch (err) {
       return res.status(500).json({
         ok: 0,
         message: err.toString(),
-      })
+      });
     }
   },
 
   getAllInfo: async (req, res) => {
-    let { page, limit, sort, order, notSupplied } = req.query
-    const _page = Number(page) || 1
-    const _limit = limit ? parseInt(limit) : 10
-    const _offset = (_page - 1) * _limit
-    const _sort = sort || 'id'
-    const _order = order || 'DESC'
-    const _notSupplied = Boolean(notSupplied) || false
-    const now = new Date()
-    now.setHours(0)
-    now.setMinutes(0)
-    now.setSeconds(0)
-    const today = now.getTime()
+    let { page, limit, sort, order, notSupplied } = req.query;
+    const _page = Number(page) || 1;
+    const _limit = limit ? parseInt(limit) : 10;
+    const _offset = (_page - 1) * _limit;
+    const _sort = sort || 'id';
+    const _order = order || 'DESC';
+    const _notSupplied = Boolean(notSupplied) || false;
+    const now = new Date().toUTCString();
+    const today = new Date(now).getTime();
     try {
       const products = await Product.findAndCountAll({
         where: {
@@ -315,32 +303,29 @@ const productController = {
         limit: _limit,
         offset: _offset,
         order: [[_sort, _order]],
-      })
+      });
       return res.status(200).json({
         ok: 1,
         data: products,
-      })
+      });
     } catch (err) {
       return res.status(500).json({
         ok: 0,
         message: err.toString(),
-      })
+      });
     }
   },
 
   searchByKeyword: async (req, res) => {
-    let { page, limit, sort, order, keyword, notSupplied } = req.query
-    const _page = Number(page) || 1
-    const _limit = limit ? parseInt(limit) : 10
-    const _offset = (_page - 1) * _limit
-    const _sort = sort || 'id'
-    const _order = order || 'DESC'
-    const _notSupplied = Boolean(notSupplied) || false
-    const now = new Date()
-    now.setHours(0)
-    now.setMinutes(0)
-    now.setSeconds(0)
-    const today = now.getTime()
+    let { page, limit, sort, order, keyword, notSupplied } = req.query;
+    const _page = Number(page) || 1;
+    const _limit = limit ? parseInt(limit) : 10;
+    const _offset = (_page - 1) * _limit;
+    const _sort = sort || 'id';
+    const _order = order || 'DESC';
+    const _notSupplied = Boolean(notSupplied) || false;
+    const now = new Date().toUTCString();
+    const today = new Date(now).getTime();
     try {
       const productCategories = await ProductCategory.findAll({
         where: {
@@ -348,10 +333,10 @@ const productController = {
             [Op.substring]: keyword,
           },
         },
-      })
+      });
       const productCategoriesList = productCategories.map(
         (category) => category.id
-      )
+      );
       const products = await Product.findAndCountAll({
         where: {
           isAvailable: true,
@@ -403,19 +388,19 @@ const productController = {
         limit: _limit,
         offset: _offset,
         order: [[_sort, _order]],
-      })
+      });
       if (!products) {
-        throw new Error()
+        throw new Error();
       }
       return res.status(200).json({
         ok: 1,
         data: products,
-      })
+      });
     } catch (err) {
       return res.status(500).json({
         ok: 0,
         message: err.toString(),
-      })
+      });
     }
   },
 
@@ -425,9 +410,9 @@ const productController = {
         return res.status(500).json({
           ok: 0,
           message: err.toString(),
-        })
+        });
       }
-      const vendorId = decoded.payload.vendorId
+      const vendorId = decoded.payload.vendorId;
       const {
         name,
         categoryId,
@@ -437,11 +422,11 @@ const productController = {
         expiryDate,
         description,
         isAvailable,
-      } = req.body
-      const picture = req.file
+      } = req.body;
+      const picture = req.file;
       try {
         if (picture) {
-          const encodeImage = picture.buffer.toString('base64')
+          const encodeImage = picture.buffer.toString('base64');
           uploadImg(encodeImage, album, async (err, link) => {
             const product = await Product.create({
               vendorId,
@@ -454,23 +439,23 @@ const productController = {
               expiryDate,
               description,
               isAvailable,
-            })
+            });
             return res.status(200).json({
               ok: 1,
               message: 'Success',
               data: {
                 productId: product.id,
               },
-            })
-          })
+            });
+          });
         }
       } catch (err) {
         return res.status(500).json({
           ok: 0,
           message: err.toString(),
-        })
+        });
       }
-    })
+    });
   },
 
   updateProduct: async (req, res) => {
@@ -479,10 +464,10 @@ const productController = {
         return res.status(500).json({
           ok: 0,
           message: err.toString(),
-        })
+        });
       }
-      const vendorId = decoded.payload.vendorId
-      const id = req.params.id
+      const vendorId = decoded.payload.vendorId;
+      const id = req.params.id;
       const {
         name,
         categoryId,
@@ -492,67 +477,68 @@ const productController = {
         expiryDate,
         description,
         isAvailable,
-      } = req.body
-      const picture = req.file
+      } = req.body;
+      const picture = req.file;
       try {
         const updateProduct = await Product.findOne({
           where: {
             id,
             vendorId,
           },
-        })
+        });
         if (picture) {
           deleteImg(updateProduct.pictureUrl, (err) => {
             if (err) {
               return res.status(500).json({
                 ok: 0,
                 message: err.toString(),
-              })
+              });
             }
-          })
-          const encodeImage = picture.buffer.toString('base64')
+          });
+          const encodeImage = picture.buffer.toString('base64');
           uploadImg(encodeImage, album, async (err, link) => {
-            updateProduct.pictureUrl = link
-            if (name) updateProduct.name = name
-            if (categoryId) updateProduct.categoryId = categoryId
-            if (price) updateProduct.price = price
-            if (quantity) updateProduct.quantity = quantity
-            if (manufactureDate) updateProduct.manufactureDate = manufactureDate
-            if (expiryDate) updateProduct.expiryDate = expiryDate
-            if (description) updateProduct.description = description
-            if (isAvailable) updateProduct.isAvailable = isAvailable
+            updateProduct.pictureUrl = link;
+            if (name) updateProduct.name = name;
+            if (categoryId) updateProduct.categoryId = categoryId;
+            if (price) updateProduct.price = price;
+            if (quantity) updateProduct.quantity = quantity;
+            if (manufactureDate)
+              updateProduct.manufactureDate = manufactureDate;
+            if (expiryDate) updateProduct.expiryDate = expiryDate;
+            if (description) updateProduct.description = description;
+            if (isAvailable) updateProduct.isAvailable = isAvailable;
 
-            await updateProduct.save()
+            await updateProduct.save();
             return res.status(200).json({
               ok: 1,
               message: 'Success',
               data: {
                 productId: updateProduct.id,
               },
-            })
-          })
+            });
+          });
         } else {
-          if (name) updateProduct.name = name
-          if (categoryId) updateProduct.categoryId = categoryId
-          if (price) updateProduct.price = price
-          if (quantity) updateProduct.quantity = quantity
-          if (manufactureDate) updateProduct.manufactureDate = manufactureDate
-          if (expiryDate) updateProduct.expiryDate = expiryDate
-          if (description) updateProduct.description = description
-          if (isAvailable) updateProduct.isAvailable = isAvailable
-          await updateProduct.save()
+          if (name) updateProduct.name = name;
+          if (categoryId) updateProduct.categoryId = categoryId;
+          if (price) updateProduct.price = price;
+          if (quantity) updateProduct.quantity = quantity;
+          if (manufactureDate) updateProduct.manufactureDate = manufactureDate;
+          if (expiryDate) updateProduct.expiryDate = expiryDate;
+          if (description) updateProduct.description = description;
+          if (isAvailable) updateProduct.isAvailable = isAvailable;
+          await updateProduct.save();
           return res.status(200).json({
             ok: 1,
             message: 'Success',
-          })
+          });
         }
       } catch (err) {
         return res.status(500).json({
           ok: 0,
           message: err.toString(),
-        })
+        });
       }
-    })
+    });
   },
 
   deleteProduct: async (req, res) => {
@@ -561,51 +547,51 @@ const productController = {
         return res.status(500).json({
           ok: 0,
           message: err.toString(),
-        })
+        });
       }
-      const vendorId = decoded.payload.vendorId
-      const id = req.params.id
+      const vendorId = decoded.payload.vendorId;
+      const id = req.params.id;
       try {
         await Product.destroy({
           where: {
             id,
             vendorId,
           },
-        })
+        });
         return res.status(200).json({
           ok: 1,
           message: 'Success',
-        })
+        });
       } catch (err) {
         return res.status(500).json({
           ok: 0,
           message: err.toString(),
-        })
+        });
       }
-    })
+    });
   },
 
   getCartData: async (req, res) => {
     try {
-      const cart = req.body.cart
-      const cartProductsId = []
+      const cart = req.body.cart;
+      const cartProductsId = [];
       cart.forEach((item) => {
-        cartProductsId.push(item.id)
-      })
+        cartProductsId.push(item.id);
+      });
 
       const groupByVendor = (data) => {
         const groupedData = data.reduce((groups, product) => {
           for (let i = 0; i < cart.length; i++) {
             if (product.id === cart[i].id) {
-              product.dataValues['cartQuantity'] = cart[i].quantity
+              product.dataValues['cartQuantity'] = cart[i].quantity;
             }
           }
-          groups[product.vendorId] = groups[product.vendorId] || []
-          groups[product.vendorId].push(product)
-          return groups
-        }, {})
-        return groupedData
-      }
+          groups[product.vendorId] = groups[product.vendorId] || [];
+          groups[product.vendorId].push(product);
+          return groups;
+        }, {});
+        return groupedData;
+      };
       const rawCartData = await Product.findAll({
         where: {
           id: cartProductsId,
@@ -620,20 +606,20 @@ const productController = {
             attributes: ['id', 'vendorName', 'avatarUrl', 'categoryId'],
           },
         ],
-      })
-      const cartData = groupByVendor(rawCartData)
+      });
+      const cartData = groupByVendor(rawCartData);
 
       return res.status(200).json({
         ok: 1,
         data: cartData,
-      })
+      });
     } catch (err) {
       return res.status(500).json({
         ok: 0,
         message: err.toString(),
-      })
+      });
     }
   },
-}
+};
 
-module.exports = productController
+module.exports = productController;
